5c66aa8dc52b72db04d75dafce7c4c6f3c12f906
Eric Kelly <heroiceric@gmail.com>
2014-09-02 16:17:03 -0400

    Working tabs

---
+++ public/assets/javascripts/app.js
@@ -0,0 +1,17 @@
+$(function() {
+  $('.tabs-nav a').on('click', function(event) {
+    // Prevent the link from being followed (stay on same page)
+    event.preventDefault();
+
+    // Update current tab
+    var $currentTab = $(this);
+    $currentTab.addClass('active');
+    $currentTab.siblings().removeClass('active');
+
+    // Update current tab pane
+    var currentTabPaneSelector = $currentTab.attr('href');
+    var $currentTabPane = $(currentTabPaneSelector);
+    $currentTabPane.addClass('active');
+    $currentTabPane.siblings().removeClass('active');
+  });
+});
